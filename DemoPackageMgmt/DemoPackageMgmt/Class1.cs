31124e95a4b6e4c2e0a6d47b7609f51728048e45
sadhna.mahur <sadhna.mahur@BDC4A-D-2VTS6C2>
2018-06-11 12:40:42 +0530

    Initial Solution

---
+++ DemoPackageMgmt/DemoPackageMgmt/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoPackageMgmt
+{
+    public class Class1
+    {
+        int Addition(int a , int b)
+        {
+            return a + b;
+        }
+    }
+}
